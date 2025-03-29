351cf50c8e96035c75c8dae16222631fe076e5b1
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2025-03-29 22:51:02 +0100

    Data loading

--- ImageSegmentation/myCode.py
+++ ImageSegmentation/myCode.py
@@ -7,3 +7,7 @@ from tensorflow.keras.layers import (Flatten, Dense, Reshape, Conv2D, MaxPool2D,
 import matplotlib.pyplot as plt
 
 plt.rcParams['image.cmap'] = 'Greys_r'
+
+# loading the training and test sets from TFRecords
+raw_training_dataset = tf.data.TFRecordDataset('data/train_images.tfrecords')
+raw_val_dataset      = tf.data.TFRecordDataset('data/val_images.tfrecords')

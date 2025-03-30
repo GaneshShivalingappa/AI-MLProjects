ace31638f2d7640b4074f1ef0d51d49106640fc9
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2025-03-30 23:37:36 +0200

    added function read_and_decode

--- ImageSegmentation/myCode.py
+++ ImageSegmentation/myCode.py
@@ -11,3 +11,35 @@ plt.rcParams['image.cmap'] = 'Greys_r'
 # loading the training and test sets from TFRecords
 raw_training_dataset = tf.data.TFRecordDataset('data/train_images.tfrecords')
 raw_val_dataset      = tf.data.TFRecordDataset('data/val_images.tfrecords')
+
+# dictionary describing the fields stored in TFRecord, and used to extract the date from the TFRecords
+image_feature_description = {
+    'height':    tf.io.FixedLenFeature([], tf.int64),
+    'width':     tf.io.FixedLenFeature([], tf.int64),
+    'depth':     tf.io.FixedLenFeature([], tf.int64),
+    'name' :     tf.io.FixedLenFeature([], tf.string),
+    'image_raw': tf.io.FixedLenFeature([], tf.string),
+    'label_raw': tf.io.FixedLenFeature([], tf.string),
+}
+
+# helper function to extract an image from the dictionary
+def _parse_image_function(example_proto):
+    return tf.io.parse_single_example(example_proto, image_feature_description)
+
+parsed_training_dataset = raw_training_dataset.map(_parse_image_function)
+parsed_val_dataset      = raw_val_dataset.map(_parse_image_function)
+
+# function to read and decode an example from the parsed dataset
+@tf.function
+def read_and_decode(example):
+    image_raw = tf.io.decode_raw(example['image_raw'], tf.int64)
+    image_raw.set_shape([65536])
+    image = tf.reshape(image_raw, [256, 256, 1])
+
+    image = tf.cast(image, tf.float32) * (1. / 1024)
+
+    label_raw = tf.io.decode_raw(example['label_raw'], tf.uint8)
+    label_raw.set_shape([65536])
+    label = tf.reshape(label_raw, [256, 256, 1])
+
+    return image, label

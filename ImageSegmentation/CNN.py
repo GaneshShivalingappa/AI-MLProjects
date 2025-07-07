b0ee381673442b6f41df91b83fd2646c3e8dcd0c
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2025-07-07 23:31:57 +0200

    CNN model

---
+++ ImageSegmentation/CNN.py
@@ -0,0 +1,64 @@
+import datetime
+import os
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import (Flatten, Dense, Reshape, Conv2D, MaxPool2D, Conv2DTranspose)
+import matplotlib.pyplot as plt
+
+plt.rcParams['image.cmap'] = 'Greys_r'
+
+# loading the training and test sets from TFRecords
+raw_training_dataset = tf.data.TFRecordDataset('ImageSegmentation/data/train_images.tfrecords')
+raw_val_dataset      = tf.data.TFRecordDataset('ImageSegmentation/data/val_images.tfrecords')
+
+# dictionary describing the fields stored in TFRecord, and used to extract the date from the TFRecords
+image_feature_description = {
+    'height':    tf.io.FixedLenFeature([], tf.int64),
+    'width':     tf.io.FixedLenFeature([], tf.int64),
+    'depth':     tf.io.FixedLenFeature([], tf.int64),
+    'name' :     tf.io.FixedLenFeature([], tf.string),
+    'image_raw': tf.io.FixedLenFeature([], tf.string),
+    'label_raw': tf.io.FixedLenFeature([], tf.string),
+}
+
+# helper function to extract an image from the dictionary
+def _parse_image_function(example_proto):
+    return tf.io.parse_single_example(example_proto, image_feature_description)
+
+parsed_training_dataset = raw_training_dataset.map(_parse_image_function)
+parsed_val_dataset      = raw_val_dataset.map(_parse_image_function)
+
+# function to read and decode an example from the parsed dataset
+@tf.function
+def read_and_decode(example):
+    image_raw = tf.io.decode_raw(example['image_raw'], tf.int64)
+    image_raw.set_shape([65536])
+    image = tf.reshape(image_raw, [256, 256, 1])
+    image = tf.cast(image, tf.float32) * (1. / 1024)
+    label_raw = tf.io.decode_raw(example['label_raw'], tf.uint8)
+    label_raw.set_shape([65536])
+    label = tf.reshape(label_raw, [256, 256, 1])
+    return image, label
+
+# get datasets read and decoded, and into a state usable by TensorFlow
+tf_autotune = tf.data.experimental.AUTOTUNE
+train = parsed_training_dataset.map(read_and_decode, num_parallel_calls=tf_autotune)
+val = parsed_val_dataset.map(read_and_decode)
+#train.element_spec
+
+# setup the buffer size and batch size for data reading and training
+BUFFER_SIZE = 10
+BATCH_SIZE = 1
+
+# setup the train and test data by shuffling, prefetching, etc
+train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_dataset = train_dataset.prefetch(buffer_size=tf_autotune)
+test_dataset  = val.batch(BATCH_SIZE)
+
+# set up the model architecture
+model = tf.keras.models.Sequential([
+    Flatten(input_shape=[256, 256, 1]),
+    Dense(64, activation='relu'),
+    Dense(256*256*2, activation='softmax'),
+    Reshape((256, 256, 2))
+])

0f1bf39c19102c80bc410bbfc4f55472876289bd
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2025-11-20 23:03:38 +0100

    data loading for MNIST

--- GAN/GAN.py
+++ GAN/GAN.py
@@ -1,14 +1,19 @@
 import os
 os.environ.setdefault('TF_ENABLE_ONEDNN_OPTS', '0')
 os.environ.setdefault('TF_CPP_MIN_LOG_LEVEL', '2')
-
 import tensorflow as tf
-import glob
-import imageio
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
 import time
-from IPython import display
-from tqdm import tqdm
 
-layers = tf.keras.layers
+# --- CONFIGURATION ---
+BATCH_SIZE = 256
+NOISE_DIM = 100
+EPOCHS = 50
+
+# --- LOAD DATA (MNIST) ---
+(train_images, _), (_, _) = tf.keras.datasets.mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+
+

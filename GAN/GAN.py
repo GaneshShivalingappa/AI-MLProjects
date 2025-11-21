21f90a296164892087f15c28b70c3ef2af97270f
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2025-11-21 23:42:41 +0100

    Add generator model

--- GAN/GAN.py
+++ GAN/GAN.py
@@ -16,4 +16,25 @@ EPOCHS = 50
 (train_images, _), (_, _) = tf.keras.datasets.mnist.load_data()
 train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
 
+train_images = (train_images - 127.5) / 127.5  # Normalize to [-1, 1]
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(60000).batch(BATCH_SIZE)
 
+# --- BUILD GENERATOR ---
+def build_generator():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(NOISE_DIM,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+
+    return model

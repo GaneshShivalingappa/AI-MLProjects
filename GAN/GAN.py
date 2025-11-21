faef938d1e44494be1cb5d1716038f81a9b37083
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2025-11-22 00:30:37 +0100

    Add build_discriminator function

--- GAN/GAN.py
+++ GAN/GAN.py
@@ -38,3 +38,21 @@ def build_generator():
     model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
 
     return model
+
+# --- BUILD DISCRIMINATOR ---
+def build_discriminator():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model    
+
+
